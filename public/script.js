fa379cd0e34661801037985b2ca3b634a5898b8b
zepto-0321 <yiyoushiye@gmail.com>
2025-07-11 22:48:07 +0900

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,108 @@
+//ｷｬｰｯのぞき見よ！！！！
+const socket = io();
+
+var username = localStorage.getItem("username");//ユーザーネームを取得
+var room = localStorage.getItem("room");
+
+if (!username) {//なければユーザーネームを聞く
+  username = prompt("plz enter username here...") || "NONAME";
+  localStorage.setItem("username", username);
+}
+if(!room){//ルーム情報がなければルームを聞く
+  room = prompt("plz enter room name here...");
+  localStorage.setItem("room",room);
+}
+
+document.getElementById("room").textContent='Room:'+localStorage.getItem('room');
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const messages = document.getElementById("messages");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();//ページのリロードを停止
+  if (input.value) {
+   //サーバーにdetaオブジェクトを送信(Chat messageイベントを起こす)
+   socket.emit("Chat message", {username:username,message:input.value,room:room});
+   input.value = "";//入力欄を空に入力欄を空に
+  }
+});
+
+socket.emit("join room",{username:username,room:room});//roomに入ったことを知らせる入ったことを知らせる
+
+socket.on("Chat message", function(deta) {
+  const item = document.createElement("li");//作成
+  item.textContent = `${deta.username}:${deta.message}`;//detaのuserとmessageを取得し、つなげる
+  messages.appendChild(item);
+  messages.scrollTop = messages.scrollHeight;//スクロール
+});
+
+socket.on("roomList", function(roomArray) {
+  const roomList = document.getElementById("roomList");
+  roomList.innerHTML = ""; // 既存のリストをクリア
+  
+  const isRoomSet = !!localStorage.getItem("room");
+
+  // リストが空で、roomが未設定なら prompt で聞く
+  if (!isRoomSet && roomArray.length === 0) {
+    const inputRoom = prompt("plz enter room name here...")?.trim();//trim()で余計な空白を削除
+    if (inputRoom) {
+      localStorage.setItem("room", inputRoom);
+      location.reload();
+    }
+    return;
+  }
+
+
+  
+  roomArray.forEach(roomName => {//roomArrayの長さだけ繰り返す
+    const item = document.createElement("li");
+    item.textContent = roomName;
+    item.className="roomElm"
+    item.style.cursor = "pointer";
+    item.onclick = function() {
+      if (confirm(`Join room: ${roomName}?`)) {//liクリック時にroomに入ることを確認
+        localStorage.setItem("room", roomName);
+        location.reload();
+      }
+    };
+    roomList.appendChild(item);
+  });
+});
+
+function chgusm(){
+  username = prompt('New user name:');
+  localStorage.removeItem("username");
+  localStorage.setItem("username",username);  
+  location.reload();
+}
+function chgroom(){
+  room = prompt('Room:');
+  localStorage.removeItem("room");
+  localStorage.setItem("room",room);
+  location.reload();
+}
+function sendImg(){
+  const sendImg = document.getElementById("sendImg");
+  const fileInput = document.getElementById("fileInput");
+  sendImg.addEventListener('click',()=>{
+    fileInput.click();//#sendImgクリック時に、#fileInputをクリック
+  });
+  
+  fileInput.addEventListener('change',(event)=>{
+    const file = event.target.files[0];//ファイルを代入
+    if(!file) return;//ファイルじゃないなら引き返す
+   
+    const reader = new FileReader();//FileReaderを作成
+    reader.onload=function(e){
+      const base64 = e.target.result;
+      const imgHTML = document.createElement("img");//imgを作成
+      imgHTML.src=base64;//src要素を変更
+      imgHTML.maxWidth="500px";
+      imgHTML.maxHeight="500px";
+      messages.appendChild(imgHTML);
+    };
+    reader.readAsDataURL(file);
+    
+  });
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app); //httpサーバー作成
+const io = require("socket.io")(http);
+
+var roomList = new Set();
+
+const PORT = process.env.PORT || 3000;
+//ポートか、ローカル時には3000を使用
+
+app.use(express.static("public"));
+//publicフォルダ内のファイルを使用
+
+io.on("connection",(socket)=>{
+  console.log("User Connected!");
+  socket.on("join room",({room,username})=>{//roomとusernameを受け渡して
+    socket.join(room);                      //roomに参加
+    roomList.add(room);                     //Setに追加(配列とは違い、重複はできない)
+    io.emit("roomList",Array.from(roomList)); //roomListを送信(配列に変換)
+    console.log(`${username} joined in ${room}` );
+  });
+  socket.on("Chat message",(msg)=>{//Chat message イベント発生時
+    io.to(msg.room).emit("Chat message",msg);//msg.roomだけにmsgを送信
+  });
+  
+  socket.on("disconnecting",()=>{//切断時(正確には切断する直前)
+    for (var room of socket.rooms) {
+      if (room !== socket.id) {
+        var clients = io.sockets.adapter.rooms.get(room);//その部屋に属する人のSetを取得を取得
+        //io.emit("roomUsers",clients.size);
+        if (!clients || clients.size === 1) {//切断する直前が一人=>切断したら0人でルームを削除
+          roomList.delete(room);//部屋削除
+        }
+      }
+    }
+  io.emit("roomList", Array.from(roomList));
+    console.log("user disconnected!");
+  });
+});
+
+http.listen(PORT,()=>{//サーバーの起動
+  console.log("Server is running on port:"+PORT);
+});
